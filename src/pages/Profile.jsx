4fc14b15fbc5371d2634150fb229fe1bc9562bc4
MaxJGruber <maxjgruber@gmail.com>
2020-10-23 09:22:36 +0200

    brushing up on responsive design

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -82,73 +82,75 @@ class Profile extends React.Component {
   render() {
     // console.log(this.context);
     return (
-      <div className="background-form edit">
-        <div className="form-container">
-          <form className="Form">
-            <img
-              src={this.state.profileImage}
-              alt="profile-pic"
-              className="profile-photo"
-            />
-            <label htmlFor="profileImage">Profile Image</label>
-            <input
-              type="file"
-              id="profileImage"
-              name="profileImage"
-              onChange={this.handleChange}
-            />
-            <label htmlFor="firstName">First Name</label>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              defaultValue={this.state.firstName}
-              onChange={this.handleChange}
-            />
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              defaultValue={this.state.lastName}
-              onChange={this.handleChange}
-            />
-            <Link to="/loglist" id="logs">
-              <button>Check Logs</button>
-            </Link>
-            <br />
-            <div className="option-btns">
-              <Button
-                type="button"
-                id="delete"
-                className="button"
-                onClick={this.open}
-              >
-                <FontAwesomeIcon icon={faTrashAlt} />
-              </Button>
-              <Confirm
-                open={this.state.open}
-                onCancel={this.close}
-                onConfirm={this.deleteAccount}
-              />{" "}
-              <button id="save" onClick={this.handleEdit}>
-                Save Changes
-              </button>{" "}
-              <Button
-                type="button"
-                id="logout"
-                className="button"
-                onClick={this.open}
-              >
-                <FontAwesomeIcon icon={faSignOutAlt} />
-              </Button>
-              <Confirm
-                open={this.state.open}
-                onCancel={this.close}
-                onConfirm={this.sessionLogout}
+      <div className="upperScreen">
+        <div className="background-form edit">
+          <div className="form-container">
+            <form className="Form">
+              <img
+                src={this.state.profileImage}
+                alt="profile-pic"
+                className="profile-photo"
               />
-            </div>
-          </form>
+              <label htmlFor="profileImage">Profile Image</label>
+              <input
+                type="file"
+                id="profileImage"
+                name="profileImage"
+                onChange={this.handleChange}
+              />
+              <label htmlFor="firstName">First Name</label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                defaultValue={this.state.firstName}
+                onChange={this.handleChange}
+              />
+              <label htmlFor="lastName">Last Name</label>
+              <input
+                type="text"
+                id="lastName"
+                name="lastName"
+                defaultValue={this.state.lastName}
+                onChange={this.handleChange}
+              />
+              <Link to="/loglist" id="logs">
+                <button>Check Logs</button>
+              </Link>
+              <br />
+              <div className="option-btns">
+                <Button
+                  type="button"
+                  id="delete"
+                  className="button"
+                  onClick={this.open}
+                >
+                  <FontAwesomeIcon icon={faTrashAlt} />
+                </Button>
+                <Confirm
+                  open={this.state.open}
+                  onCancel={this.close}
+                  onConfirm={this.deleteAccount}
+                />{" "}
+                <button id="save" onClick={this.handleEdit}>
+                  Save Changes
+                </button>{" "}
+                <Button
+                  type="button"
+                  id="logout"
+                  className="button"
+                  onClick={this.open}
+                >
+                  <FontAwesomeIcon icon={faSignOutAlt} />
+                </Button>
+                <Confirm
+                  open={this.state.open}
+                  onCancel={this.close}
+                  onConfirm={this.sessionLogout}
+                />
+              </div>
+            </form>
+          </div>
         </div>
         <NavMain page="edit-profile" />
       </div>
